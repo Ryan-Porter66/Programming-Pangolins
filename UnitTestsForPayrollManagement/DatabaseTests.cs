2909fb46b5637555edaf056ed1bab12dee477637
Blake Humes <blake2616@hotmail.com>
2022-11-14 21:22:02 -0500

    Added Database class and login method, along with unit test for login method.

---
+++ UnitTestsForPayrollManagement/DatabaseTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PayrollManagement.Classes;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace UnitTestsForPayrollManagement
+{
+    [TestClass]
+    public class DatabaseTests
+    {
+        [TestMethod]
+        public void TestLogin()
+        {
+            // Note: This only passes when the webserver is turned on. Ping Blake in discord if it doesn't pass.
+            try
+            {
+                string username = "1";
+                string password = "thisaintit";
+
+                string privilege = Database.login(username, password);
+
+                Assert.AreEqual("Denied", privilege);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+            
+        }
+    }
+}
