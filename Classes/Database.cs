3e7a2eb835fbb4961d7a5783fca98b3cd817f748
Blake Humes <blake2616@hotmail.com>
2022-11-22 16:00:23 -0500

    Added method addEmp to Database class and webserer file.

--- Classes/Database.cs
+++ Classes/Database.cs
@@ -3,11 +3,72 @@ using System.Net;
 using System.Text;
 using System.IO;
 using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace PayrollManagement.Classes
 {
     public static class Database
     {
+        public static string addEmployee(string username, string passwordHash, Employee emp)
+        {
+            string enc = "yHdeGH1Dl56vt28/Rdi+PvnWWqz62EEB/dBA2aMLOWw8PRtHzGk1VUcsSjX/Y9Q6jtQT8SYykjQof6/6Wrid2dcjWAr2ZSwjBSPKLC6OctU=";
+            string url = Encryption.AESDecryption(enc);
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+
+            var postData = "username=" + Uri.EscapeDataString(username);
+            postData += "&password=" + Uri.EscapeDataString(passwordHash);
+            postData += "&firstName=" + Uri.EscapeDataString(emp.FirstName);
+            postData += "&lastName=" + Uri.EscapeDataString(emp.LastName);
+            postData += "&ssn=" + Uri.EscapeDataString(emp.Ssn);
+            postData += "&dob=" + Uri.EscapeDataString(emp.Dob.ToString("yyyy-mm-dd"));
+            postData += "&phonenumber=" + Uri.EscapeDataString(emp.PhoneNumber);
+            postData += "&hiredate=" + Uri.EscapeDataString(emp.HireDate.ToString("yyyy-mm-dd"));
+
+            if (emp is HourlyEmployee hourlyEmployee)
+            {
+                postData += "&hourlyrate=" + Uri.EscapeDataString(hourlyEmployee.PayPerHour.ToString());
+                postData += "&salary=" + Uri.EscapeDataString("0");
+                postData += "&exempt=" + Uri.EscapeDataString("0");
+            }
+            else if(emp is SalaryEmployee salaryEmployee)
+            {
+                postData += "&salary=" + Uri.EscapeDataString(salaryEmployee.SalaryPerPayPeriod.ToString());
+                postData += "&hourlyrate=" + Uri.EscapeDataString("0");
+                postData += "&exempt=" + Uri.EscapeDataString("1");
+            }
+            string pwordHash = Encryption.SHA256Encryption(emp.Ssn.Substring(Math.Max(emp.Ssn.Length - 4, 0)));
+            postData += "&passwordhash=" + Uri.EscapeDataString(pwordHash);
+            postData += "&permissionlevel=" + Uri.EscapeDataString(emp.Permissions);
+            postData += "&street=" + Uri.EscapeDataString(emp.Address);
+            postData += "&city=" + Uri.EscapeDataString(emp.City);
+            postData += "&state=" + Uri.EscapeDataString(emp.State);
+            postData += "&postalcode=" + Uri.EscapeDataString(emp.PostalCode);
+            postData += "&bankname=" + Uri.EscapeDataString(emp.Bank.BankName);
+            postData += "&routingnum=" + Uri.EscapeDataString(emp.Bank.BankRoutingNumber);
+            postData += "&accountnum=" + Uri.EscapeDataString(emp.Bank.BankAccountNumber);
+            postData += "&department=" + Uri.EscapeDataString(emp.Department);
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string responseText = reader.ReadToEnd();
+
+            var Jobj = (JObject)JsonConvert.DeserializeObject(responseText);
+            string empid = Jobj["emp_id"].ToString();
+
+            return empid;
+        }
         public static string login(string username, string passwordHash)
         {
             string enc = "yHdeGH1Dl56vt28/Rdi+PvnWWqz62EEB/dBA2aMLOWw8PRtHzGk1VUcsSjX/Y9Q6i3OiUOCQ0y/r2UPf0MRlhAokL+J37IkP3jUs3f1lZig=";
--- Server/webserver.js
+++ Server/webserver.js
@@ -10,10 +10,92 @@ app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-app.post('/auth', async function (request, response) {
+async function dbconnect(u, p) {
+    let user = process.env.USR;
+    let pwd = process.env.PASSWORD;
+    let db = process.env.DATABASE;
+    let server = process.env.SERVER;
+
+    const config = {
+        user: user,
+        password: pwd,
+        host: server,
+        database: db
+    };
+
+    const statement = "INSERT INTO test VALUES('" + u + "', '" + p + "');";
+
+    var con = mysql.createConnection(config);
+
+    con.connect(function (err) {
+        if (err) console.log(err);
+        console.log("Connected!");
+        con.query(statement, function (err, result) {
+            if (err) console.log(err);
+            console.log("Result: " + result);
+        });
+    });
+
+
+}
+
+async function login(u, p) {
+    let user = process.env.USR;
+    let pwd = process.env.PASSWORD;
+    let db = process.env.DATABASE;
+    let server = process.env.SERVER;
+
+
+    const config = {
+        user: user,
+        password: pwd,
+        host: server,
+        database: db
+    };
+
+    const statement = "SELECT * FROM employees WHERE emp_id = ?;";
+
+    var con = mysql.createConnection(config);
+    var privelege = 'Error';
+    var temp = await con.connect(async function (err) {
+        if (err) console.log(err);
+        console.log("Connected!");
+        return await con.query(statement, u, function (err, result) {
+            if (err) console.log(err);
+            console.log("Result: " + result[0].password_hash);
+            if (p === result[0].password_hash)
+                privelege = result[0].permission_level;
+            else
+                privelege = 'Denied';  
+            return privelege;
+        });
+    });
+    return privelege;
+}
+app.post('/addemp', async function (request, response) {
     // Capture the input fields
     let username = request.body.username;
     let password = request.body.password;
+    let firstName = request.body.firstName;
+    let lastName = request.body.lastName;
+    let ssn = request.body.ssn;
+    let dob = request.body.dob;
+    let phonenumber = request.body.phonenumber;
+    let hiredate = request.body.hiredate;
+    let hourlyrate = request.body.hourlyrate;
+    let salary = request.body.salary;
+    let exempt = request.body.exempt;
+    let passwordhash = request.body.passwordhash;
+    let permissionlevel = request.body.permissionlevel;
+    let street = request.body.street;
+    let city = request.body.city;
+    let state = request.body.state;
+    let postalcode = request.body.postalcode;
+    let bankname = request.body.bankname;
+    let routingnum = request.body.routingnum;
+    let accountnum = request.body.accountnum;
+    let department = request.body.department;
+
     // Ensure the input fields exists and are not empty
     if (username && password) {
 
@@ -24,33 +106,35 @@ app.post('/auth', async function (request, response) {
 
 
         const config = {
+            connectionLimit: 25,
             user: user,
             password: pwd,
             host: server,
             database: db
         };
 
-        const statement = "SELECT * FROM employees WHERE emp_id = ?;";
+        const statement = "call add_emp(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
 
-        var con = mysql.createConnection(config);
-        var privilege = 'Error';
-        con.connect(function (err) {
-            if (err) console.log(err);
-            console.log("Connected!");
-            con.query(statement, username, function (err, result) {
-                if (err) {
-                    console.log(err);
-                    response.send(privilege);
-                    response.end();
-                }
-                if (password === result[0].password_hash)
-                    privilege = result[0].permission_level;
-                else
-                    privelege = 'Denied';
-                response.send(privilege);
+        const values = [firstName, lastName, ssn, dob, phonenumber, hiredate, exempt, hourlyrate,
+                        salary, passwordhash, permissionlevel, street, city, state, postalcode,
+                        bankname, routingnum, accountnum, department];
+
+        var pool = mysql.createPool(config);
+        var empid = "-99";
+        pool.query(statement, values, function (err, result) {
+            if (err) {
+                console.log(err);
+                response.send(empid);
                 response.end();
+            }
+            if (password === password)
+                empid = result[0][0].emp_id;
+            else
+                empid = '-999';
+            console.log(empid);
+            response.json(result[0][0]);
+            response.end();
 
-            });
         });
 
     } else {
@@ -59,11 +143,57 @@ app.post('/auth', async function (request, response) {
     }
 });
 
+app.post('/auth', async function (request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+    if (username && password) {
+
+        let user = process.env.USR;
+        let pwd = process.env.PASSWORD;
+        let db = process.env.DATABASE;
+        let server = process.env.SERVER;
+
+
+        const config = {
+            connectionLimit: 25,
+            user: user,
+            password: pwd,
+            host: server,
+            database: db
+        };
+
+        const statement = "SELECT * FROM employees WHERE emp_id = ?;";
+
+        var pool = mysql.createPool(config);
+        var privelege = 'Error';
+        pool.query(statement, username, function (err, result) {
+            if (err) {
+                console.log(err);
+                response.send(privelege);
+                response.end();
+            }
+            if (password === result[0].password_hash)
+                privelege = result[0].permission_level;
+            else
+                privelege = 'Denied';
+            response.send(privelege);
+            response.end();
+
+        });
+
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+    }
+});
+
 let port = 5000;
 //let port = process.env.PORT;
 if (port == null || port == "") {
-    port = 5000;
+	port = 5000;
 }
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+	console.log(`Listening on port ${port}`);
 });
