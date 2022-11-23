ac3cafa2dec091b4f8ed81fd69d9f47c461a38c0
Blake Humes <blake2616@hotmail.com>
2022-11-23 17:47:53 -0500

    Updated employee table and AddEmployee method to accept fed tax rate. Also hopefully fixed the webserver erroring out after a few hours.

--- Classes/Database.cs
+++ Classes/Database.cs
@@ -5,12 +5,45 @@ using System.IO;
 using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace PayrollManagement.Classes
 {
     public static class Database
     {
-        public static string addEmployee(string username, string passwordHash, Employee emp)
+        /*public static Employee GetEmployee(string username, string passwordHash, int empID)
+        {
+            string enc = "yHdeGH1Dl56vt28/Rdi+PvnWWqz62EEB/dBA2aMLOWw8PRtHzGk1VUcsSjX/Y9Q6jtQT8SYykjQof6/6Wrid2dcjWAr2ZSwjBSPKLC6OctU=";
+            string url = Encryption.AESDecryption(enc);
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+
+            var postData = "username=" + Uri.EscapeDataString(username);
+            postData += "&password=" + Uri.EscapeDataString(passwordHash);
+            postData += "&empid=" + Uri.EscapeDataString(empID.ToString());
+
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string responseText = reader.ReadToEnd();
+
+            var Jobj = (JObject)JsonConvert.DeserializeObject(responseText);
+            string empid = Jobj["emp_id"].ToString();
+
+
+        }*/
+
+        public static string AddEmployee(string username, string passwordHash, Employee emp)
         {
             string enc = "yHdeGH1Dl56vt28/Rdi+PvnWWqz62EEB/dBA2aMLOWw8PRtHzGk1VUcsSjX/Y9Q6jtQT8SYykjQof6/6Wrid2dcjWAr2ZSwjBSPKLC6OctU=";
             string url = Encryption.AESDecryption(enc);
@@ -38,6 +71,7 @@ namespace PayrollManagement.Classes
                 postData += "&hourlyrate=" + Uri.EscapeDataString("0");
                 postData += "&exempt=" + Uri.EscapeDataString("1");
             }
+            postData += "&fedtax=" + Uri.EscapeDataString(emp.FederalTaxRate.ToString());
             string pwordHash = Encryption.SHA256Encryption(emp.Ssn.Substring(Math.Max(emp.Ssn.Length - 4, 0)));
             postData += "&passwordhash=" + Uri.EscapeDataString(pwordHash);
             postData += "&permissionlevel=" + Uri.EscapeDataString(emp.Permissions);
@@ -69,7 +103,7 @@ namespace PayrollManagement.Classes
 
             return empid;
         }
-        public static string login(string username, string passwordHash)
+        public static string Login(string username, string passwordHash)
         {
             string enc = "yHdeGH1Dl56vt28/Rdi+PvnWWqz62EEB/dBA2aMLOWw8PRtHzGk1VUcsSjX/Y9Q6i3OiUOCQ0y/r2UPf0MRlhAokL+J37IkP3jUs3f1lZig=";
             string url = Encryption.AESDecryption(enc);
--- UnitTestsForPayrollManagement/DatabaseTests.cs
+++ UnitTestsForPayrollManagement/DatabaseTests.cs
@@ -16,10 +16,10 @@ namespace PayrollManagementUnitTests
             // Note: This only passes when the webserver is turned on. Ping Blake in discord if it doesn't pass.
             try
             {
-                string username = "1";
+                string username = "2";
                 string password = "thisaintit";
 
-                string privilege = Database.login(username, password);
+                string privilege = Database.Login(username, password);
 
                 Assert.AreEqual("Denied", privilege);
             }
@@ -36,7 +36,7 @@ namespace PayrollManagementUnitTests
             // Note: This only passes when the webserver is turned on. Ping Blake in discord if it doesn't pass.
             try
             {
-                string username = "1";
+                string username = "2";
                 string password = "thisaintit";
                 DateTime dob = new DateTime(1990, 1, 2);
                 DateTime hire = new DateTime(2019, 3, 14);
@@ -47,14 +47,37 @@ namespace PayrollManagementUnitTests
                     new PercentageDeduction("Percentage", 0.03m)
                 };
                 HourlyEmployee emp = new HourlyEmployee("John", "Doe", "101 N 57th St", "Springfield"
-                    , "10101", "MI", bank, 0001, .25m, "all", "101-12-1234", dob, hire, "407-98-2134", "IT", tempDeduct, 0.06m, 19.99m);
+                    , "10101", "MI", bank, 0001, .25m, "Employee", "101-12-1234", dob, hire, "407-98-2134", "IT", tempDeduct, 0.06m, 19.99m);
 
-                string resp = Database.addEmployee(username, password, emp);
+                string resp = Database.AddEmployee(username, password, emp);
                 Console.WriteLine(resp);
                 // addEmployee returns the emp ID of the newly created employee, so the id is printed to the console and the database is checked
                 // 
                 Assert.AreEqual(1, 1);
                 
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+        }
+
+        [TestMethod]
+        public void TestGetEmployee()
+        {
+            // Note: This only passes when the webserver is turned on. Ping Blake in discord if it doesn't pass.
+            try
+            {
+                string username = "2";
+                string password = "thisaintit";
+
+                //Employee resp = Database.GetEmployee(username, password, 1);
+                //Console.WriteLine(resp);
+                // addEmployee returns the emp ID of the newly created employee, so the id is printed to the console and the database is checked
+                // 
+                Assert.AreEqual(1, 1);
+
             }
             catch (Exception ex)
             {
