49a10b8b49747e6543faf14c3c5a18fe8475ac5c
evanslominski <evan_slominski@icloud.com>
2022-11-27 20:03:31 -0600

    Starting to build the Login Page -No real functionality yet -Just testing passing variables

---
+++ Forms/LoginPage.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PayrollManagement.Forms
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.PayrollProgramTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TextBoxUsername = new System.Windows.Forms.TextBox();
+            this.TextBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.BackColor = System.Drawing.Color.Silver;
+            this.LoginButton.Location = new System.Drawing.Point(797, 827);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(256, 93);
+            this.LoginButton.TabIndex = 0;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = false;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton1);
+            // 
+            // PayrollProgramTextBox
+            // 
+            this.PayrollProgramTextBox.BackColor = System.Drawing.Color.Silver;
+            this.PayrollProgramTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PayrollProgramTextBox.Location = new System.Drawing.Point(500, 52);
+            this.PayrollProgramTextBox.Name = "PayrollProgramTextBox";
+            this.PayrollProgramTextBox.Size = new System.Drawing.Size(936, 152);
+            this.PayrollProgramTextBox.TabIndex = 1;
+            this.PayrollProgramTextBox.Text = "Payroll Program";
+            this.PayrollProgramTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Silver;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(500, 272);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(491, 67);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Please Login Below:";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Silver;
+            this.pictureBox1.Location = new System.Drawing.Point(500, 355);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(936, 439);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TextBoxUsername
+            // 
+            this.TextBoxUsername.Location = new System.Drawing.Point(534, 447);
+            this.TextBoxUsername.Name = "TextBoxUsername";
+            this.TextBoxUsername.Size = new System.Drawing.Size(412, 31);
+            this.TextBoxUsername.TabIndex = 5;
+            this.TextBoxUsername.TextChanged += new System.EventHandler(this.UsernameTextBox);
+            // 
+            // TextBoxPassword
+            // 
+            this.TextBoxPassword.Location = new System.Drawing.Point(534, 584);
+            this.TextBoxPassword.Name = "TextBoxPassword";
+            this.TextBoxPassword.Size = new System.Drawing.Size(412, 31);
+            this.TextBoxPassword.TabIndex = 6;
+            this.TextBoxPassword.TextChanged += new System.EventHandler(this.PasswordTextbox);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(534, 390);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(166, 44);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "Username:";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(534, 534);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(166, 44);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "Password:";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1903, 1049);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.TextBoxPassword);
+            this.Controls.Add(this.TextBoxUsername);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PayrollProgramTextBox);
+            this.Controls.Add(this.LoginButton);
+            this.Name = "LoginPage";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.LoginPage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.TextBox PayrollProgramTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox TextBoxUsername;
+        private System.Windows.Forms.TextBox TextBoxPassword;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ Forms/LoginPage.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows.Forms;
+
+namespace PayrollManagement.Forms
+{
+    public partial class LoginPage : Form
+    {
+        #region Auto-Built 
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginPage_Load(object sender, System.EventArgs e)
+        {
+
+        }
+        #endregion
+        #region Dynamic Methods
+        private void LoginButton1(object sender, System.EventArgs e)
+        {
+            string username;
+            string password;
+
+            username = TextBoxUsername.Text;
+            password = TextBoxPassword.Text;
+            var combo = username + ":" + password;
+
+            MessageBox.Show(combo, "Test");
+
+        }
+        private void UsernameTextBox(object sender, System.EventArgs e)
+        {
+
+        }
+        private void PasswordTextbox(object sender, System.EventArgs e)
+        {
+
+        }
+        #endregion
+        #region Static Methods
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
